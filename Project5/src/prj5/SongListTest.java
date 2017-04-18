782141b1f6183842b04353cfccc7eb16caf4d527
Mitchellliou <mliou@vt.edu>
2017-04-17 21:13:06 -0400

    Add files via upload

--- Project5/src/prj5/SongListTest.java
+++ Project5/src/prj5/SongListTest.java
@@ -1,12 +1,13 @@
+
 package prj5;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import student.TestCase;
 
-public class SongListTest extends TestCase
-{
+public class SongListTest extends TestCase {
 
-  //~ Fields--------------------
+    // ~ Fields--------------------
     private Student charlie;
     private Student cynthia;
     private Student vikram;
@@ -21,53 +22,54 @@ public class SongListTest extends TestCase
     private Song irresistible;
 
     private SongList list;
+
+
     /**
      * sets up the test with a SongList, 4 students
      * and 7 songs that are added to the list
      */
-    public void setUp()
-    {
+    public void setUp() {
         charlie = new Student("ME", "Thailand", "squatting");
         cynthia = new Student("CS", "China", "coughing");
         mitchell = new Student("CS", "NOVA", "kungfu fighting");
         vikram = new Student("Bio", "Little Asia", "eating");
-        
+
         perfect = new Song("Perfect", "Ed Sheeran", 2017, "Pop");
         perfect.addToHeards(charlie);
         perfect.addToHeards(vikram);
         perfect.addToHeards(cynthia);
         perfect.addToLikes(charlie);
         perfect.addToLikes(vikram);
-        
+
         dive = new Song("Dive", "Ed Sheeran", 2017, "Pop");
         dive.addToHeards(charlie);
         dive.addToLikes(charlie);
-        
+
         dont = new Song("Dont", "Ed Sheeran", 2015, "Pop");
         dont.addToHeards(charlie);
         dont.addToLikes(charlie);
-        
+
         closer = new Song("Closer", "Chainsmokers", 2015, "Pop");
         closer.addToHeards(charlie);
         closer.addToHeards(vikram);
         closer.addToHeards(mitchell);
         closer.addToHeards(cynthia);
         closer.addToLikes(charlie);
-        
+
         royals = new Song("Royals", "Lorde", 2014, "Pop");
         royals.addToHeards(charlie);
         royals.addToHeards(cynthia);
         royals.addToLikes(cynthia);
-        
+
         starving = new Song("Starving", "Hailee Steinfeld", 2015, "Pop");
         starving.addToHeards(charlie);
         starving.addToLikes(charlie);
-        
+
         smile = new Song("Smile", "Uncle Kracker", 2010, "Pop");
         smile.addToHeards(charlie);
-        
+
         irresistible = new Song("Irresistible", "Fall Out Boy", 2015, "Rock");
-        
+
         list = new SongList();
         list.add(perfect);
         list.add(royals);
@@ -76,19 +78,21 @@ public class SongListTest extends TestCase
         list.add(starving);
         list.add(smile);
         list.add(dive);
-        
+
     }
+
 
     /**
      * tests if the method adds correctly
      */
     public void testAdd() {
         assertEquals(7, list.getSize());
-        
+
         list.add(irresistible);
         assertEquals(8, list.getSize());
-        
+
     }
+
 
     /**
      * tests if it returns true when list is empty
@@ -100,6 +104,7 @@ public class SongListTest extends TestCase
         assertFalse(list2.isEmpty());
     }
 
+
     /**
      * tests if this method clears the list correctly
      */
@@ -110,12 +115,25 @@ public class SongListTest extends TestCase
         assertEquals(0, list.getSize());
     }
 
+
     /**
      * tests if toString outputs the right string
      */
     public void testToString() {
-        //pain in the ass to test
+        SongList testList = new SongList();
+        assertEquals("Song List of 0 songs.\n", testList.toString());
+        testList.add(closer);
+        assertEquals("Song List of 1 songs.\n" + "SONG 1 - Song Title: Closer\n"
+            + "Song Artist: Chainsmokers\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n", testList.toString());
+        testList.add(dive);
+        assertEquals("Song List of 2 songs.\n" + "SONG 1 - Song Title: Closer\n"
+            + "Song Artist: Chainsmokers\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 2 - Song Title: Dive\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n", testList.toString());
     }
+
 
     /**
      * tests if the iterator iterates through the list correctly
@@ -132,44 +150,98 @@ public class SongListTest extends TestCase
         assertEquals(smile, it.next());
         assertEquals(starving, it.next());
         assertFalse(it.hasNext());
-        
+
         Exception exception = null;
-        try
-        {
+        try {
             it.next();
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             exception = e;
         }
         assertNotNull(exception);
         assertTrue(exception instanceof NoSuchElementException);
-        
+
         SongList list2 = new SongList();
         Iterator<Song> it2 = list2.iterator();
         assertFalse(it2.hasNext());
     }
-    
+
+
     /**
      * tests if the songs sort in the correct order
      * for each type of sort
      */
     public void testSortSongs() {
+        // Sort by title
         list.sortSongs(SortTypeEnum.title);
-        System.out.println("Sort by title");
-        System.out.println(list.toString());
-        
+        assertEquals("Song List of 7 songs.\n" + "SONG 1 - Song Title: Closer\n"
+            + "Song Artist: Chainsmokers\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 2 - Song Title: Dive\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 3 - Song Title: Dont\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 4 - Song Title: Perfect\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 5 - Song Title: Royals\n"
+            + "Song Artist: Lorde\n" + "Song Genre: Pop\n" + "Song Year: 2014\n"
+            + "SONG 6 - Song Title: Smile\n" + "Song Artist: Uncle Kracker\n"
+            + "Song Genre: Pop\n" + "Song Year: 2010\n"
+            + "SONG 7 - Song Title: Starving\n"
+            + "Song Artist: Hailee Steinfeld\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n", list.toString());
+
+        // Sort by artist
         list.sortSongs(SortTypeEnum.artist);
-        System.out.println("Sort by artist");
-        System.out.println(list.toString());
-        
+        assertEquals("Song List of 7 songs.\n" + "SONG 1 - Song Title: Closer\n"
+            + "Song Artist: Chainsmokers\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 2 - Song Title: Dive\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 3 - Song Title: Dont\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 4 - Song Title: Perfect\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 5 - Song Title: Starving\n"
+            + "Song Artist: Hailee Steinfeld\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 6 - Song Title: Royals\n"
+            + "Song Artist: Lorde\n" + "Song Genre: Pop\n" + "Song Year: 2014\n"
+            + "SONG 7 - Song Title: Smile\n" + "Song Artist: Uncle Kracker\n"
+            + "Song Genre: Pop\n" + "Song Year: 2010\n", list.toString());
+
+        // Sort by genre
         list.sortSongs(SortTypeEnum.genre);
-        System.out.println("Sort by genre");
-        System.out.println(list.toString());
-        
+        assertEquals("Song List of 7 songs.\n" + "SONG 1 - Song Title: Smile\n"
+            + "Song Artist: Uncle Kracker\n" + "Song Genre: Pop\n"
+            + "Song Year: 2010\n" + "SONG 2 - Song Title: Royals\n"
+            + "Song Artist: Lorde\n" + "Song Genre: Pop\n" + "Song Year: 2014\n"
+            + "SONG 3 - Song Title: Starving\n"
+            + "Song Artist: Hailee Steinfeld\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 4 - Song Title: Perfect\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 5 - Song Title: Dont\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 6 - Song Title: Dive\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 7 - Song Title: Closer\n"
+            + "Song Artist: Chainsmokers\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n", list.toString());
+
+        // Sort by year
         list.sortSongs(SortTypeEnum.year);
-        System.out.println("Sort by year");
-        System.out.println(list.toString());
+        assertEquals("Song List of 7 songs.\n" + "SONG 1 - Song Title: Smile\n"
+            + "Song Artist: Uncle Kracker\n" + "Song Genre: Pop\n"
+            + "Song Year: 2010\n" + "SONG 2 - Song Title: Royals\n"
+            + "Song Artist: Lorde\n" + "Song Genre: Pop\n" + "Song Year: 2014\n"
+            + "SONG 3 - Song Title: Closer\n" + "Song Artist: Chainsmokers\n"
+            + "Song Genre: Pop\n" + "Song Year: 2015\n"
+            + "SONG 4 - Song Title: Dont\n" + "Song Artist: Ed Sheeran\n"
+            + "Song Genre: Pop\n" + "Song Year: 2015\n"
+            + "SONG 5 - Song Title: Starving\n"
+            + "Song Artist: Hailee Steinfeld\n" + "Song Genre: Pop\n"
+            + "Song Year: 2015\n" + "SONG 6 - Song Title: Dive\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n" + "SONG 7 - Song Title: Perfect\n"
+            + "Song Artist: Ed Sheeran\n" + "Song Genre: Pop\n"
+            + "Song Year: 2017\n", list.toString());
     }
 
 }
